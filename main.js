041453db6bb020cc47008ce95539df0b46fe8fdb
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2023-11-10 10:41:43 -0300

    bfdb

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ListaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < ListaDeTeclas,length){
-const efeito = ListaDeTeclas[contador].classList [1]
+const efeito = ListaDeTeclas[contador].classList[1]
 const idAudio = "#som_"+efeito;
 ListaDeTeclas[contador].onlclick = function(){
     tocasom (idAudio)
