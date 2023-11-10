ab2ed21ff1b11d788b0e0e1f69c4bccbc472ca10
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2023-11-10 10:42:55 -0300

    jry

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function tocasom(idElementoaudio){
 const ListaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador < ListaDeTeclas,length){
+while(contador < ListaDeTeclas.length){
 const efeito = ListaDeTeclas[contador].classList[1]
 const idAudio = "#som_"+efeito;
 ListaDeTeclas[contador].onlclick = function(){
