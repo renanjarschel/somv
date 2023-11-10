23bbcd5ae04c3555e288c78112b2c3d80109e42f
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2023-11-10 10:36:48 -0300

    yjt

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 function tocasom(idElementoaudio){
     document.querySelector(idElementoaudio).play();
 }
-const ListaDeTeclas = 
+const ListaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < ListaDeTeclas,length){
+const efeito = ListaDeTeclas[contador].classList [1]
+const idAudio = "#som_"+efeito;
+ListaDeTeclas[contador].onlclick = function(){
+    tocasom (idAudio)
+}
+    contador = contador +1;
+    contador.log (contador)
+}
